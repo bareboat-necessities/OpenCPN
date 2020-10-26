1e86c0332c829dc3cbc56d51a44d16ca3baee3a4
mgrouch <mgrushinskiy@gmail.com>
2020-10-26 09:41:53 -0400

    wx 3.1

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -61,7 +61,7 @@ docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install autotools-d
     wx3.1-i18n
 
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
-    "update-alternatives --set fakeroot /usr/bin/fakeroot-tcp; cd ci-source; dpkg-buildpackage -b -uc -us -j4; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist"
+    "update-alternatives --set fakeroot /usr/bin/fakeroot-tcp; cd ci-source; dpkg-buildpackage -b -uc -us -j2; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist"
 
 find dist -name \*.\*$EXT
 
